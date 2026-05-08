3616cb97f55d104f6c51fb4d78798cfbce4d1956
laoshihao <1765030326@qq.com>
2026-05-08 18:22:14 +0800

    feat:线上情况错误收集

--- vue-source/src/main.ts
+++ vue-source/src/main.ts
@@ -1,13 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
+import {findErrBySourceMap } from './utils/index.js'
 import App from './App.vue'
 import router from './router/index.ts'
 import ErrorStackParser from 'error-stack-parser'
 
 const app = createApp(App)
 
-app.config.errorHandler = (err, instance, info) => {
+app.config.errorHandler = (err) => {
     const errorParser = ErrorStackParser.parse(err as Error)
+    findErrBySourceMap(errorParser[0])
     console.log(errorParser)
 }
 
